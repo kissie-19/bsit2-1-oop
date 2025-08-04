4657cbf74c1e18c0ddd1c31c005ebd39f5f6885b
Applie Kissie <kaaput42160@liceo.edu.ph>
2025-08-04 10:26:14 +0800

    lab-activity-3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        Book book1 = new Book("Little Women", "Louisa May Alcott", 500);
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", 258);
+        Book book3 = new Book("Catching Fire", "Suzanne Collins", 562);
+
+        System.out.println();
+
+        book1.displayInfo();
+        System.out.println();
+        book2.displayInfo();
+        System.out.println();
+        book3.displayInfo();
+        System.out.println();
+
+        System.out.println();
+
+        book3.borrowBook();
+        book3.displayInfo();
+        System.out.println();
+        book2.returnBook();
+        book2.displayInfo();
+    }
+}
+
+class Book {
+    String title;
+    String author;
+    int pages;
+    boolean isAvailable;
+
+    Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isAvailable = true;
+        System.out.println("A new book " + title + " by " + author + " has been added to the library!");
+    }
+
+    void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Available: " + isAvailable);
+    }
+
+    void borrowBook() {
+        isAvailable = false;
+        System.out.println("" + title + " has been borrowed.");
+    }
+
+    void returnBook() {
+        isAvailable = true;
+        System.out.println("" + title + " has been returned.");
     }
 }
