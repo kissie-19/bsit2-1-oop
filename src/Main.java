c33a57a41e435d9e4789c641138ad56570bf6d67
KISSIE ANN APPLE APUT <kaaput42160@liceo.edu.ph>
2025-08-19 11:56:56 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        System.out.println("\nBank Name: " + BankAccount.bankName);
+        System.out.println("Interest Rate: 3.0%\n");
+
+        BankAccount account1 = new BankAccount("John Doe", 1000.0);
+        BankAccount account2 = new BankAccount("Jane Smith", 2500.0);
+        BankAccount account3 = new BankAccount("Bob Johnson", 500.0);
+
+        System.out.println("\n== Account Operations ==");
+        account1.deposit(500.0);
+        account2.withdraw(300.0);
+
+        System.out.println("\n== Interest Calculation ==");
+
+        System.out.println("John Deo's interest: $" + account1.calculateInterest());
+        System.out.println("Jane Smith's interest: $" + account2.calculateInterest());
+        System.out.println("Bob Johnson's interest: $" + account3.calculateInterest());
+
+
+        System.out.println("\nTotal Accounts Created: " + BankAccount.totalAccounts);
     }
 }
