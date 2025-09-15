e874e74d80f7bace787448164ec642d497aedfb4
Kissie Ann Apple C. Aput <kaaput42160@liceo.edu.ph>
2025-09-15 10:01:56 +0800

    lab-activity-9

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        PetService service = new PetService();
+
+
+        System.out.println("---- Pet Service Fees ----");
+        System.out.println("Basic Checkup: $" + service.calculateFee());
+        System.out.println("Checkup with Vaccination: $" + service.calculateFee(true));
+        System.out.println("Full Service (Vaccination + Grooming): $" + service.calculateFee(true, true));
+        System.out.println("Emergency Service: $" + service.calculateFee("emergency"));
+
+        System.out.println("\n---- Welcome to Pet Clinic! ----");
+        Pet dog = new Dog("Buddy", 3);
+        Pet cat = new Cat("Whiskers", 2);
+        Pet bird = new Bird("Tweety", 1);
+
+        dog.displayInfo();
+        cat.displayInfo();
+        bird.displayInfo();
+
+        System.out.println("\n---- Training Session Started ----");
+        trainPet((Trainable) dog);
+        trainPet((Trainable) bird);
+    }
+
+
+    public static void trainPet(Trainable pet) {
+        pet.performTrick();
     }
 }
