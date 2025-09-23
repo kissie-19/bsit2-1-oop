c8909d01b280e3d763e8feb957669c162532fd03
Kissie Ann Apple C. Aput <kaaput42160@liceo.edu.ph>
2025-09-23 11:15:56 +0800

    lab-activity-9

--- src/Bird.java
+++ src/Bird.java
@@ -1,16 +1,16 @@
-public class Bird extends Pet implements Trainable {
+    public class Bird extends Pet implements Trainable {
 
-    public Bird(String name, int age) {
-        super(name, age);
-    }
+        public Bird(String name, int age) {
+            super(name, age);
+        }
 
-    @Override
-    public String makeSound() {
-        return "Tweet! Tweet!";
-    }
+        @Override
+        public String makeSound() {
+            return "Tweet! Tweet!";
+        }
 
-    @Override
-    public void performTrick() {
-        System.out.println("Training " + name + ": Flies in circles and lands on perch");
+        @Override
+        public void performTrick() {
+            System.out.println("Training " + name + ": Flies in circles and lands on perch");
+        }
     }
-}
